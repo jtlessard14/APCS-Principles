cac936313a28b1e920cab81fca694695d43b37d8
jtlessard14 <5801762@FPSMA.net>
2016-09-13 09:42:42 -0400

    Lab03v100Lessard
    Lab 3

---
+++ Lab03v100Lessard.py
@@ -0,0 +1,27 @@
+# Lab03v100Lessard.py
+# The First Program
+# This is the Student Starting version of Lab03.
+#
+print("REQUIRED FOR 80 & 100 POINT VERSIONS")   
+print("()")
+print("***************************************")
+print("LAB 03A, THE FIRST PROGRAM")
+print("100 POINT VERSION")
+print("BY JON LESSARD")
+print("***************************************")
+print("()")
+print("A poet I am not")
+print("Don't claim to be")
+print("But what I wrote")
+print("Did come from me")
+
+print("REQUIRED FOR 100 POINT VERSION")
+x = 10
+y = 3
+print (x + y)
+print (x - y)
+print (x * y)
+print (x / y)
+print (x // y)
+print (x % y)
+print (x ** y)
